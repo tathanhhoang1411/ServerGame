0510cb4632391068b9cf5d9fe6c26bb907bb4144
HOÀNG TẠ <tathanhhoang.work@gmail.com>
2023-12-28 22:01:25 +0700

    complete

---
+++ AppLuckyNumber(Server)/AppLuckyNumber(Server)/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace AppLuckyNumber_Server_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLuckyNumber = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotalPlayer = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(93, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Xổ Số Con Gà Trống";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(1, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Con số trúng thưởng lượt trước";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLuckyNumber
+            // 
+            this.lblLuckyNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLuckyNumber.AutoSize = true;
+            this.lblLuckyNumber.Font = new System.Drawing.Font("Monotype Corsiva", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLuckyNumber.ForeColor = System.Drawing.Color.Coral;
+            this.lblLuckyNumber.Location = new System.Drawing.Point(282, 104);
+            this.lblLuckyNumber.Name = "lblLuckyNumber";
+            this.lblLuckyNumber.Size = new System.Drawing.Size(166, 97);
+            this.lblLuckyNumber.TabIndex = 2;
+            this.lblLuckyNumber.Text = "Chờ";
+            this.lblLuckyNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Coral;
+            this.label4.Location = new System.Drawing.Point(0, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(180, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số người tham gia";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalPlayer
+            // 
+            this.lblTotalPlayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotalPlayer.AutoSize = true;
+            this.lblTotalPlayer.Font = new System.Drawing.Font("Monotype Corsiva", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPlayer.ForeColor = System.Drawing.Color.Coral;
+            this.lblTotalPlayer.Location = new System.Drawing.Point(337, 277);
+            this.lblTotalPlayer.Name = "lblTotalPlayer";
+            this.lblTotalPlayer.Size = new System.Drawing.Size(0, 52);
+            this.lblTotalPlayer.TabIndex = 4;
+            this.lblTotalPlayer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Coral;
+            this.label3.Location = new System.Drawing.Point(0, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Thời gian:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Monotype Corsiva", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.Coral;
+            this.lblTime.Location = new System.Drawing.Point(370, 390);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 21);
+            this.lblTime.TabIndex = 6;
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Coral;
+            this.label5.Location = new System.Drawing.Point(1, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(425, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Sẽ được làm mới số may mắn vào khung giờ kế tiếp";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Orange;
+            this.label11.LiveSetting = System.Windows.Forms.Automation.AutomationLiveSetting.Assertive;
+            this.label11.Location = new System.Drawing.Point(277, 411);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(271, 20);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "By: tathanhhoang.work@gmail.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(597, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTotalPlayer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblLuckyNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SERVER XỔ SỐ CON GÀ TRỐNG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLuckyNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotalPlayer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ AppLuckyNumber(Server)/AppLuckyNumber(Server)/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xNet;
+using Label = System.Windows.Forms.Label;
+
+namespace AppLuckyNumber_Server_
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComponent1();
+            int timeCycle = 60;
+            WaitAndPost(timeCycle);
+        }
+        private void InitializeComponent1()
+        {
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // Cập nhật mỗi giây
+            timer.Tick += timer1_Tick;
+            timer.Start();
+        }
+        private async Task WaitAndPost(int timeCycle)
+        {
+            try
+            {
+                xNet.HttpRequest http1 = new xNet.HttpRequest();
+                xNet.HttpRequest http2 = new xNet.HttpRequest();
+                Thread t = new Thread(async () =>
+                {
+                    while (true)
+                    {
+                        Random random = new Random();
+                        int luckyNumber = random.Next(0, 9);
+                        int minute1 = DateTime.Now.Minute;
+                        //if (minute1 == 0 || minute1 == 1 || minute1 == 2 )//Đôi khi thao tác chậm, nên cho khoảng thời gian này 
+                        //{
+                            List<string> list = new List<string>(); 
+                            int a = int.Parse( http1.Post("http://tahoang111-001-site1.btempurl.com/api/Check/PostLuckyNumber?luckyNumber=" + luckyNumber).ToString());
+                            if (a != -1)
+                            {
+                                //Đọc tất cả file của người chơi để thưởng coin
+                                string allFilePlayer = http2.Get("http://tahoang111-001-site1.btempurl.com/api/Check/AllFilePlayer?code=amkjdfiuhfd").ToString();
+                                 if (allFilePlayer.Contains(".txt"))
+                                {
+                                    var arr = allFilePlayer.Split(',');
+                                    foreach(string item in arr)
+                                    {
+                                        var arrItem = item.Split('\\');
+                                        string item1 = arrItem[arrItem.Length-1];
+                                        var arrItemChild= item1.Split('_');
+                                        if(arrItemChild[1].Contains(luckyNumber.ToString()))
+                                        {
+                                            list.Add(arrItemChild[0]);
+                                        }
+                                    }   
+                                }
+                                foreach(string item in list) //danh sách IDVi
+                                { 
+                                string resultGetCoin = http2.Post("http://tahoang111-001-site1.btempurl.com/api/Walletes/GetCoin?id="+ item).ToString();
+                                }
+                                //Xóa file txt của người chơi đã cược 
+                                string deleteFilePlayer = http2.Post("http://tahoang111-001-site1.btempurl.com/api/Check/DeleteFile?code=amkjdfiuhfd").ToString();
+                            }
+                        //}
+                        int minute2 = DateTime.Now.Minute;
+                        Thread.Sleep((timeCycle - minute2) * 60 * 1000);
+                        //Thread.Sleep(timeCycle * 60 * 1000);
+                    }
+                });
+                t.Start();
+            }
+            catch
+            {
+
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                lblTime.Text = DateTime.Now.ToString("HH:mm:ss, dd-MM-yyyy"); // Định dạng ngày giờ đầy đủ
+            }
+            catch
+            {
+
+            }
+        }
+        //load số may mắn lên giao diện
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread thread1 = new Thread(UpdateLblTotalPlayer);
+            thread1.Start();
+            Thread thread2 = new Thread(UpdateLblLKNumber);
+            thread2.Start();
+        }
+        private void UpdateLblLKNumber()
+        {
+            try
+            {
+                xNet.HttpRequest http = new xNet.HttpRequest();
+                while (true)
+                {
+                    int minute = DateTime.Now.Minute;
+                    int result = int.Parse(http.Get("http://tahoang111-001-site1.btempurl.com/api/Check/LuckyNumber").ToString());
+                    UpdateLblLKNumberValue(result.ToString());
+                    //Thread.Sleep((60 - minute) * 60 * 1000);
+                    Thread.Sleep(10000);//10 giây gửi yêu cầu 1 lần
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void UpdateLblTotalPlayer()
+        {
+            try
+            {
+                xNet.HttpRequest http = new xNet.HttpRequest();
+                while (true)
+                {
+                    int result = int.Parse(http.Get("http://tahoang111-001-site1.btempurl.com/api/Check/CountPlayerCurrent").ToString());
+                    UpdateLblTotalPlayerValue(result.ToString());
+                    Thread.Sleep(10000);//10 giây gửi yêu cầu 1 lần
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        //gắn tổng người cược vào lable
+        private void UpdateLblTotalPlayerValue(string value)
+        {
+            if (lblTotalPlayer.InvokeRequired)
+            {
+                // Truy cập và cập nhật Label thông qua UI thread
+                lblTotalPlayer.Invoke((MethodInvoker)(() => lblTotalPlayer.Text = value));
+            }
+            else
+            {
+                lblTotalPlayer.Text = value;
+            }
+        }     
+        //gắn số may mắn vào lable
+        private void UpdateLblLKNumberValue(string value)
+        {
+            if (lblLuckyNumber.InvokeRequired)
+            {
+                // Truy cập và cập nhật Label thông qua UI thread
+                lblLuckyNumber.Invoke((MethodInvoker)(() => lblLuckyNumber.Text = value));
+            }
+            else
+            {
+                lblLuckyNumber.Text = value;
+            }
+        }
+    }
+}
